04b3e2b40c18a90d30320f3d17859efdef40b25f
ericbanker12 <ericbanker12@gmail.com>
2019-10-28 14:18:27 -0500

    create server, create POST endpoint, error if POST without name or bio

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+const express = require('express')
+
+const port = 5000
+
+const server = express()
+
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    // console.log(req.body)
+    const {name, bio} = req.body
+    if (!name || !bio) res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+})
+
+server.listen(port, () => console.log(`\n=== Listening on port ${port} ===\n`))
