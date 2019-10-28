29e2839956ca2fe9cd83a0799ac1adbb08188861
ericbanker12 <ericbanker12@gmail.com>
2019-10-28 14:43:43 -0500

    add db, insert new user if valid, check name and bio type, allow empty string for bio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // implement your API here
 const express = require('express')
+const db = require('./data/db')
 
 const port = 5000
 
@@ -10,7 +11,28 @@ server.use(express.json())
 server.post('/api/users', (req, res) => {
     // console.log(req.body)
     const {name, bio} = req.body
-    if (!name || !bio) res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+    
+    if (!name || typeof name != 'string' || typeof bio != 'string') {
+        res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+    }
+    else {
+        const date = new Date()
+        const user = {
+            name,
+            bio,
+            created_at: date,
+            updated_at: date,
+        }
+
+        db.insert(user)
+        .then(({id}) => {
+            res.status(201).send({...user, id})
+        })
+        .catch(err => {
+            // console.log(err)
+            res.status(500).json({ error: "There was an error while saving the user to the database" })
+        })
+    }
 })
 
 server.listen(port, () => console.log(`\n=== Listening on port ${port} ===\n`))
